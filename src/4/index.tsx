fc6a43abb32c87c0ae975593bc58bd5bed1024fc
mohamedsamir <mohamedsamiredries@outlook.com>
2022-09-25 01:47:10 +0200

    Task 4 Done.

--- src/4/index.tsx
+++ src/4/index.tsx
@@ -1,33 +1,41 @@
 // Style
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState, FormEvent } from "react";
 import "./index.scss";
 
 const Task5: FunctionComponent = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const onSubmit = () => {
+  const onSubmit = (e: FormEvent) => {
     // If you want to do something with form submit
-
+    e.preventDefault();
     alert(`Email: ${email} \nPassword: ${password}`);
   };
 
   return (
     <div id="task-5">
-      <form onSubmit={onSubmit}>
-        <label>Email</label>
-        <input
-          name="email"
-          onChange={(event) => setEmail(event.currentTarget.value)}
-          value={email}
-        />
-        <label>Password</label>
-        <input
-          name="password"
-          onChange={(event) => setPassword(event.currentTarget.value)}
-          value={password}
-        />
-        <button>Login</button>
+      <form onSubmit={onSubmit} className="form">
+        <div className="form-content">
+          <div className="form-group">
+            <label className="form-label">Email</label>
+            <input
+              className="form-input"
+              name="email"
+              onChange={(event) => setEmail(event.currentTarget.value)}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Password</label>
+            <input
+              className="form-input"
+              name="password"
+              onChange={(event) => setPassword(event.currentTarget.value)}
+              value={password}
+            />
+          </div>
+          <button className="form-button">Login</button>
+        </div>
       </form>
     </div>
   );

b97f36e5dac4303607420643b2232562fa91d6d6
mohamedsamir <mohamedsamiredries@outlook.com>
2022-09-24 21:42:45 +0200

    Task 2 Done.

--- src/2/index.tsx
+++ src/2/index.tsx
@@ -3,13 +3,20 @@ import { FunctionComponent, useState } from "react";
 const Task2: FunctionComponent = () => {
   const [value, setValue] = useState("");
 
+  const handleInputChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div>
       <label>Controlled Input: </label>
-      <input />
+      <input value={value} onChange={handleInputChange} />
       <br />
       <br />
-      <p>#Show Input value here#</p>
+      <p>
+        #Show Input value here#
+        {value}
+      </p>
     </div>
   );
 };
